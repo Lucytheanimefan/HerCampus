a4a060b7c3b7145c90ea67a2a897d80e6c315bc1
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-04-02 21:21:21 -0400

    Updated facebook competitor like counts

--- Competitor Dashboard/facebookLikes.js
+++ Competitor Dashboard/facebookLikes.js
@@ -6,13 +6,21 @@
 function getLikes(access_token, pageID,callback){
 	var url='https://graph.facebook.com/v2.3/'+pageID+'?fields=likes&access_token='+access_token;
 	$.getJSON(url, function(json){
-		callback(json.likes);
+		callback(json.likes, pageIDs[json.id]);
 	})
 }
+
+var pageIDs={'113692773321':'Her Campus','6636341311':'Teen Vogue', '8028997215':'Seventeen',
+'26815555478':'Glamour',  '86973707921':'Refinery 29',  '8358247707':'Cosmopolitan', '10799255126':'Marie Claire'};
 
 var access_token='1725954970975324|sidVOHslF41oAH_KWdlnhNRuDWk'
 id='113692773321'
 
-getLikes(access_token,id,function(d){
-	document.getElementById("Facebook likes").innerHTML="Number of likes on Her Campus: "+d;
-})
+for (var key in pageIDs){
+	var div=document.getElementById("Facebook likes")
+	//div.innerHTML=div.innerHTML+"<br> Number of likes on "+key+": ";
+	getLikes(access_token,key,function(likes, name){
+		console.log(likes+" "+name)
+		div.innerHTML=div.innerHTML+"<br>"+name+": "+likes;
+	});
+}
